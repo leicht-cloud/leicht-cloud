167f23c0704d48bae4c641b2ae91a3d25cab07b5
Toon Schoenmakers <nighteyes1993@gmail.com>
2021-12-05 21:14:13 +0100

    Handle file uploads with less memory
    Instead of using ParseMultipartForm() we now use the multipart.Reader
    directly. Allowing us to read directly from the http body and write it
    directly into a storage.File. Next ideal steps would be to implement
    some form of javascript uploading, with multiple files support and a
    progress bar. Plus resumable file uploads would be a nice to have. I
    will likely keep the current way of uploading as a fallback though, as
    it works without any form of javascript, but then again should make the
    listing of files work without javascript as well in that regard.

--- pkg/http/api/upload.go
+++ pkg/http/api/upload.go
@@ -2,8 +2,11 @@ package api
 
 import (
 	"io"
+	"mime"
+	"mime/multipart"
 	"net/http"
 	"path"
+	"strings"
 
 	"github.com/schoentoon/go-cloud/pkg/auth"
 	"github.com/schoentoon/go-cloud/pkg/models"
@@ -20,31 +23,50 @@ func newUploadHandler(store storage.StorageProvider) http.Handler {
 }
 
 func (h *uploadHandler) Serve(user *models.User, w http.ResponseWriter, r *http.Request) {
-	// TODO figure out a way to upload without having to read the entire thing in memory right away..
-	err := r.ParseMultipartForm(1024 * 1024 * 8)
-	if err != nil {
-		http.Error(w, "Malformed request", http.StatusInternalServerError)
-		return
-	}
-
-	file, header, err := r.FormFile("file")
+	mediaType, params, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	f, err := h.Storage.File(r.Context(), user, path.Join("/", header.Filename))
-	if err != nil {
-		logrus.Error(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	if strings.HasPrefix(mediaType, "multipart/") {
+		mr := multipart.NewReader(r.Body, params["boundary"])
+		for {
+			p, err := mr.NextPart()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				logrus.Error(err)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			filename := p.FileName()
+			if filename == "" {
+				logrus.Error(err)
+				http.Error(w, "Empty filename?", http.StatusBadRequest)
+				return
+			}
+
+			f, err := h.Storage.File(r.Context(), user, path.Join("/", filename))
+			if err != nil {
+				logrus.Error(err)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			defer f.Close()
+
+			_, err = io.Copy(f, p)
+			if err != nil {
+				logrus.Error(err)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+
 		return
 	}
 
-	defer f.Close()
-	_, err = io.Copy(f, file)
-	if err != nil {
-		logrus.Error(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	http.Error(w, "Invalid request, expected multipart", http.StatusBadRequest)
 }
